9467d26e77c48432d0678d01647ac9dd163e1473
spacevoid2022 <spacevoid2022@gmail.com>
2026-04-08 13:38:28 -0500

    Final Auth Fix: Removed conflicting CrossOrigin tag

--- backend/src/main/java/com/app/auth/AuthController.java
+++ backend/src/main/java/com/app/auth/AuthController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
