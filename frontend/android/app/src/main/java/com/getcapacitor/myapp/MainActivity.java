78ae6c97697020cfcbcc02b8bd692a0709e1c2ab
spacevoid2022 <spacevoid2022@gmail.com>
2026-07-15 01:16:27 -0500

    Integrate AdMob SDK initialization in Android project settings

--- frontend/android/app/src/main/java/com/getcapacitor/myapp/MainActivity.java
+++ frontend/android/app/src/main/java/com/getcapacitor/myapp/MainActivity.java
@@ -1,5 +1,14 @@
 package com.getcapacitor.myapp;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.google.android.gms.ads.MobileAds;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Initialize the Google Mobile Ads SDK
+        MobileAds.initialize(this);
+    }
+}
